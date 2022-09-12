da016d5450e54570e1b19b43e800a96ced67fdec
Hanna Potapova <an.al.potapova@gmail.com>
2022-09-12 20:06:11 +0300

    Task1,2

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,41 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const imgContainer = document.querySelector('.gallery');
+const cardMarkup = createGalleryItemsMarkup(galleryItems);
+
+imgContainer.insertAdjacentHTML('beforeend', cardMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+    return galleryItems.map(({ preview, original, description }) => {
+        return `
+     <a class="gallery__item" href="${original}">
+        <img class="gallery__image" 
+        src="${preview}" 
+        alt="${description}" />
+    </a>
+     `
+    }).join("");
+};
+console.log(createGalleryItemsMarkup(galleryItems));
+
+imgContainer.addEventListener('click', onImageContainerClick);
+
+function onImageContainerClick(evt) {
+    evt.preventDefault();
+    if (evt.target.nodeName !== "IMG") {
+        return;
+    };    
+}
+
+var lightbox = new SimpleLightbox(".gallery a", {
+        captions: true,
+        captionsData: "alt",
+        captionDelay: 250,
+});
--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,14 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector("iframe");
+const player = new Player(iframe);
+const PLAYER_KEY = 'videoplayer-current-time';
+
+const savedTime = localStorage.getItem(PLAYER_KEY) || 0;
+console.log(savedTime);
+player.on("timeupdate", throttle(onPlay, 1000));
+function onPlay(data) {
+    localStorage.setItem(PLAYER_KEY, data.seconds)
+};
+player.setCurrentTime(savedTime);
